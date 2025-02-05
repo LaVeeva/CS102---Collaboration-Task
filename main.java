9451d1f8210e2175ab2bc3fe4cfecda94d023b4c
Ömür Meriç Arıcı <121347951+LaVeeva@users.noreply.github.com>
2025-02-05 19:35:09 +0300

    Update main.java

--- main.java
+++ main.java
@@ -10,4 +10,17 @@ public class main {
         scanner.close();
 
     } 
+    
+    /**
+    * Given an array size, create an int array that contains random integers in the range [0,100]. 
+    * All the remaining functionality will operate on this array.
+    * @author Ömür Meriç Arıcı
+    */
+    public static int[] createArray(int size, random Random) {
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            
+        }
+            
+    }
 }
