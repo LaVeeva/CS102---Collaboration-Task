16ec349e53a64e51a7bded6d8a9dd592300ca3bf
Ömür Meriç Arıcı <121347951+LaVeeva@users.noreply.github.com>
2025-02-05 11:06:40 +0300

    MAIN FILE
    gönder

---
+++ main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class main {
+    public static int arraySize;
+    
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
+
+    } 
+}
